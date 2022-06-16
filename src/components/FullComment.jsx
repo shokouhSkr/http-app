a3c119e9e1d79434b79d59344375e27403869877
shokouhSkr <shokouhskr@gmail.com>
2022-06-16 12:25:28 +0430

    FullComment component developed

--- src/components/FullComment.jsx
+++ src/components/FullComment.jsx
@@ -1,15 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const FullComment = ({ commentId }) => {
-  // console.log(commentId);
+  const [comment, setComment] = useState(null);
 
-  return (
-    <div className="mb-4 flex w-full flex-col items-center justify-center rounded bg-blue-200 p-8">
-      <p>name</p>
-      <p>email</p>
-      <p>body</p>
-    </div>
-  );
+  useEffect(() => {
+    if (commentId) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+        .then((res) => setComment(res.data))
+        .catch();
+    }
+  }, [commentId]);
+
+  let commentDetail = <p>Please select a comment</p>;
+
+  // console.log(commentId);
+  if (commentId) commentDetail = <p className="text-2xl text-red-600">Loading...</p>;
+
+  if (comment) {
+    commentDetail = (
+      <div className="mb-4 flex w-full flex-col items-center justify-center rounded bg-blue-200 p-8">
+        <p>{comment.name}</p>
+        <p>{comment.email}</p>
+        <p>{comment.body}</p>
+      </div>
+    );
+  }
+  return commentDetail;
 };
 
 export default FullComment;
